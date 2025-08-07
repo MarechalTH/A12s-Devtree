0f308fa2638efe725f62254d88e758efad7d6f67
TheDarkDeath788 <thedarkdeath788@gmail.com>
2025-08-07 14:14:32 -0300

    Update Android.mk

--- extra-bins/snake/Android.mk
+++ extra-bins/snake/Android.mk
@@ -4,17 +4,26 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := snake
 LOCAL_SRC_FILES := snake.c
-LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 
-# Forzar binario estático y aplicar flags personalizados
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_CFLAGS := -static -Os -flto -ffunction-sections -fdata-sections -Wl,--gc-sections -s
 
-# Hook post-compilación para comprimir con UPX
-snake_compressed := $(LOCAL_MODULE)
-$(snake_compressed): $(LOCAL_BUILT_MODULE)
-	@echo ">>> Compressing $(LOCAL_BUILT_MODULE) with UPX..."
-	$(hide) upx --ultra-brute $(LOCAL_BUILT_MODULE)
+LOCAL_CFLAGS := \
+    -Os \
+    -flto \
+    -ffunction-sections \
+    -fdata-sections \
+    -s
+
+LOCAL_LDFLAGS := \
+    -Wl,--gc-sections
+
+define compress-with-upx
+@echo ">>> Comprimiendo $(LOCAL_INSTALLED_MODULE) con UPX..."
+@# Ocultamos el comando para una salida más limpia
+$(hide) upx --ultra-brute $(LOCAL_INSTALLED_MODULE)
+endef
+
+LOCAL_POST_INSTALL_CMD := $(compress-with-upx)
 
 include $(BUILD_EXECUTABLE)
